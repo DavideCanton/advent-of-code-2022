d5189d661e87d3abd2f8f8babfc9465f7a3661fe
Davide Canton <davide.canton5@gmail.com>
2022-12-09 15:29:36 +0100

    day 3

---
+++ advent/day3/__init__.py
@@ -0,0 +1,44 @@
+import itertools as it
+from functools import cache, reduce
+
+from common import BaseAdventDay, load_asset
+
+cached_ord = cache(ord)
+
+
+class Day3(BaseAdventDay):
+    def get_score(self, letter):
+        o = ord(letter)
+        if cached_ord("a") <= o <= cached_ord("z"):
+            return o - cached_ord("a") + 1
+        elif cached_ord("A") <= o <= cached_ord("Z"):
+            return o - cached_ord("A") + 27
+        else:
+            raise ValueError(f"Invalid letter {letter}")
+
+    def get_input(self, variant) -> tuple:
+        return ([row.strip() for row in load_asset(__file__, "input.txt")],)
+
+    def run_1(self, rows):
+        def process_row(row):
+            half = len(row) // 2
+            left, right = row[:half], row[half:]
+            return frozenset(left) & frozenset(right)
+
+        return sum(max(self.get_score(c) for c in process_row(r)) for r in rows)
+
+    def run_2(self, rows):
+        tot = 0
+        for i in it.count():
+            start = 3 * i
+            if start >= len(rows):
+                break
+            group = rows[start : start + 3]
+            ret = reduce(frozenset.__and__, map(frozenset, group))
+            assert ret
+            badge = next(iter(ret))
+            tot += self.get_score(badge)
+        return tot
+
+
+Instance = Day3()
---
+++ tests/advent/day3_test.py
@@ -0,0 +1,12 @@
+import pytest
+
+from tests.advent.conftest import Base
+
+
+class TestDay2(Base):
+    DAY = 3
+    DATA = (7691, 2508)
+
+    @pytest.fixture(params=range(len(DATA)))
+    def test_cases(self, request):
+        return self._create_case(request)
